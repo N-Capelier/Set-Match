620f891f7499736a11db3ed6b0f7d1ee07eb7f43
Nicolas Capelier <capelier.nicolas@gmail.com>
2020-07-06 12:32:23 +0200

    Added Player Class & player's data

---
+++ Set & Match Compagnon/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    /// <summary>
+    /// NCO
+    /// </summary>
+    public struct PlayerID
+    {
+        public PlayerStats stats;
+        public PlayerData data;
+        public string name { get; private set; }
+        public string surname { get; private set; }
+        public string username { get; private set; }
+        public string mail { get; private set; }
+        public string password { get; private set; }
+        public bool checkedTOS { get; private set; }
+
+        public PlayerID(string name, string surname, string username, string mail, string password, bool checkedTOS)
+        {
+            stats = new PlayerStats();
+            data = new PlayerData();
+
+            this.name = name;
+            this.surname = surname;
+            this.username = username;
+            this.mail = mail;
+            this.password = password;
+            this.checkedTOS = checkedTOS;
+        }
+    }
+
+    /// <summary>
+    /// NCO
+    /// </summary>
+    public class Player : MonoBehaviour
+    {
+        #region Variables
+
+        PlayerID id;
+        bool isLoggedIn = false;
+        bool isInMatch = false;
+
+        #endregion
+
+        #region Login Methods
+
+        public bool Register(string name, string surname, string username, string mail, string password, bool checkedTOS)
+        {
+            //First, check if email address and username do not exist in database
+            //if()
+            //{
+            //    return false;
+            //}
+
+            //create ID and login
+            if(checkedTOS)
+            {
+                id = new PlayerID(name, surname, username, mail, password, checkedTOS);
+                Login(mail, password);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Login(string mail, string password)
+        {
+            isLoggedIn = true;
+
+            //goto main menu
+        }
+
+        public void Logout()
+        {
+            isLoggedIn = false;
+
+            //goto main menu
+        }
+
+        #endregion
+
+        #region Stats and data Methods
+
+        public void ResetAllData()
+        {
+            ResetStats();
+            ResetData();
+        }
+
+        public PlayerStats ResetStats()
+        {
+            return id.stats = new PlayerStats();
+        }
+
+        public PlayerData ResetData()
+        {
+            return id.data = new PlayerData();
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Set & Match Compagnon/Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerStats
+    {
+        #region Variables
+
+        float playerScore = 0;
+        float pressureCoeff = 0;
+        int victoryCount = 0;
+        int startGameService = 0;
+        int gamesPlayed = 0;
+        int hitToScore = 0;
+        int hitCount = 0;
+
+        #endregion
+
+        #region Constructors
+
+        public PlayerStats()
+        {
+            
+        }
+
+        public PlayerStats(float playerScore, float pressureCoeff, int victoryCount, int startGameService,
+            int gamesPlayed, int hitToScore, int hitCount)
+        {
+            this.playerScore = playerScore;
+            this.pressureCoeff = pressureCoeff;
+            this.victoryCount = victoryCount;
+            this.startGameService = startGameService;
+            this.gamesPlayed = gamesPlayed;
+            this.hitToScore = hitToScore;
+            this.hitCount = hitCount;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public PlayerStats Share()
+        {
+            return new PlayerStats(playerScore, pressureCoeff, victoryCount, startGameService,
+                gamesPlayed, hitToScore, hitCount);
+        }
+
+        public float VictoryRatio()
+        {
+            return victoryCount / gamesPlayed;
+        }
+
+        public float HitRatio()
+        {
+            return hitToScore / hitCount;
+        }
+
+        public float StartServiceRatio()
+        {
+            return startGameService / gamesPlayed;
+        }
+
+        #endregion
+    }
+
+}
