b1c6f01b9af19754f0c780af31ca643c8391b5e4
Deleye Arthur <arthurdeleye@gmail.com>
2020-07-21 00:08:07 +0200

    added match logic

---
+++ Set & Match Compagnon/Assets/Scripts/Match/MatchBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace TennisMatch
+{
+    /// <summary>
+    /// ARD
+    /// </summary>
+    public class MatchBehavior : MonoBehaviour
+    {
+        private MatchData matchData;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+
+
+    }
+}
