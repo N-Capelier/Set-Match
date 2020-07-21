de7cd51632b4f88278b3ea34402796f62de31d47
Deleye Arthur <arthurdeleye@gmail.com>
2020-07-21 23:07:04 +0200

    Modified RallyScoreur

--- Set & Match Compagnon/Assets/Scripts/Karp/ScoreScreen.cs
+++ Set & Match Compagnon/Assets/Scripts/Karp/ScoreScreen.cs
@@ -6,6 +6,9 @@ public class ScoreScreen : MonoBehaviour
 {
     [Header("Parametre"), SerializeField]
     float duration = 0.8f;
+    [Space(10)]
+    [SerializeField] float reductSize = 0.4f;
+    [SerializeField] float focusSize = 1.25f;
     [SerializeField] Ease easeType = Ease.InOutCubic;
     [SerializeField] Vector2 CornerPos;
     [SerializeField] Color transpColor, opaqueColor;
@@ -15,23 +18,29 @@ public class ScoreScreen : MonoBehaviour
     private void Awake()
     {
         menu = this.GetComponent<RectTransform>();
+        desact();
     }
 
     public void CornerViewport()
     {
         menu.DOAnchorPosX(CornerPos.x, duration, false).SetEase(easeType);
         menu.DOAnchorPosY(CornerPos.y, duration, false).SetEase(easeType);
-        menu.DOScale(0.3f, duration).SetEase(easeType);
+        menu.DOScale(reductSize, duration).SetEase(easeType);
         fond.DOColor(transpColor, duration).SetEase(easeType);
-
+        Invoke("desact", duration);
     }
 
     public void CenterViewport()
     {
+        fond.gameObject.SetActive(true);
         menu.DOAnchorPosX(0, duration, false).SetEase(easeType);
         menu.DOAnchorPosY(0, duration, false).SetEase(easeType);
-        menu.DOScale(1, duration).SetEase(easeType);
+        menu.DOScale(focusSize, duration).SetEase(easeType);
         fond.DOColor(opaqueColor, duration).SetEase(easeType);
+    }
 
+    private void desact()
+    {
+        fond.gameObject.SetActive(false);
     }
 }
