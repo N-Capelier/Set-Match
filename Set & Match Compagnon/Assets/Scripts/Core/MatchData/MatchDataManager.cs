13ce4b8692a6458de6ebdff8684c128204b7f93b
Deleye Arthur <arthurdeleye@gmail.com>
2020-08-21 14:43:35 +0200

    Move MatchData in Core Assembly

---
+++ Set & Match Compagnon/Assets/Scripts/Core/MatchData/MatchDataManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace TennisMatch
+{
+    /// <summary>
+    /// ARD
+    /// </summary>
+    public class MatchDataManager : Singleton<MatchDataManager>
+    {
+        [Header("MatchData")]
+        public MatchData currentMatch;
+
+        [Header("Variable")]
+        public bool rebootOnAwake = false;
+
+        private void Awake()
+        {
+            //Reset du matchData ou erreur s'il n'est pas là
+            if (currentMatch == null)
+            {
+                Debug.LogError("MatchData non attribué");
+            }
+            else
+            if (rebootOnAwake)
+            {
+                currentMatch.Reboot();
+            }
+        }
+
+
+    }
+}
--- Set & Match Compagnon/Assets/Scripts/Menu/Player/PlayerData.cs
+++ Set & Match Compagnon/Assets/Scripts/Menu/Player/PlayerData.cs
@@ -1,61 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TennisMatch;
 
-namespace Player
+public class PlayerData : MonoBehaviour
 {
-    /// <summary>
-    /// NCO
-    /// </summary>
-    public class PlayerData
+    // Start is called before the first frame update
+    void Start()
     {
-        //MatchDataUnfinished[] currentMatches = new MatchDataUnfinished[20];
-
-        #region Methods
-
-        /*public MatchDataUnfinished LoadMatch(int matchID)
-        {
-            return currentMatches[matchID];
-        }*/
         
-        /*public void DeleteMatch(int matchID)
-        {
-            for(int i = matchID; i < currentMatches.Length; i++)
-            {
-                if(i + 1 < currentMatches.Length)
-                {
-                    currentMatches[i] = currentMatches[i + 1];
-                }
-                else
-                {
-                    currentMatches[i] = null;
-                }
-            }
-        }*/
-        
-        /*public void AddMatch(MatchDataUnfinished match)
-        {
-            //Look for empty space
-            for(int i = 0; i < currentMatches.Length; i++)
-            {
-                if(currentMatches[i] == null)
-                {
-                    currentMatches[i] = match;
-                    return;
-                }
-            }
-
-            //if no empty space, move all index to n+1 and add the new match to position 0
-            for(int i = currentMatches.Length - 1; i >= 0; i--)
-            {
-                currentMatches[i] = currentMatches[i - 1];
-            }
-            currentMatches[0] = match;
-        }
-        */
-
-        #endregion
     }
 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
